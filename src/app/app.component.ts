90833564630c44cd00309195f7bfa74affc7c86c
Belette <marie.maillos@gmail.com>
2020-02-10 11:42:34 +0100

    update add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movies';
+  public title: string = 'movies';
+
+  public defaultCountry: string = "US";
+
+  public movies: any[] = [
+    {
+      title: 'Joker',
+      year: 2019,
+      country: 'US'
+    },
+    {
+      title: 'Avengers',
+      year: 2016,
+      country: 'US'
+    },
+    {
+      title: 'Il était une fois dans l\'ouest',
+      year: 1975,
+      country: "it"
+    }
+  ];
+
+  public toggleCountry(): void {
+    if (this.defaultCountry == 'US') {
+      this.defaultCountry = 'it';
+    } else {
+      this.defaultCountry = 'US';
+    }
+  }
 }
