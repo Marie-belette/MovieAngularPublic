0b746e5f9eec5eead9bf6df4d58bb9f013c3360d
Belette <marie.maillos@gmail.com>
2020-02-12 15:31:09 +0100

    afficher tous les films quand input == 0

--- src/app/pages/home/search/search.component.ts
+++ src/app/pages/home/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { NgModel, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { MovieService } from 'src/app/core/services/movie.service';
 import { take } from 'rxjs/internal/operators/take';
 import { Movie } from 'src/app/core/models/movie';
@@ -12,18 +12,48 @@ import { Movie } from 'src/app/core/models/movie';
 
 export class SearchComponent implements OnInit {
 
-  public search: string;
-
   @Output() movies : EventEmitter<Movie[]> = new EventEmitter<Movie[]>();
 
-  constructor(private movieService: MovieService) {}
+  public searchForm: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private movieService: MovieService,
+    private formBuilder: FormBuilder) {}
+
+  public get search(): AbstractControl {
+    return this.searchForm.controls.search;
+  }
+
+  ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      search: [
+        '', // Default value for the control "search"
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(2)])
+      ]
+    });
+  }
 
   public onClick() : void {
-    if (this.search.trim().length > 0) {
+    if (this.search.value.trim().length > 0) {
       let movies : Movie[] = [];
-      this.movieService.byTitle(this.search.trim())
+      this.movieService.byTitle(this.search.value.trim())
+      .pipe(take(1)) 
+      .subscribe((response: Movie[]) => {
+        movies = response.map((movie: Movie) => {
+          return new Movie().deserialize(movie);
+        });
+        console.log(`Emit : ${JSON.stringify(movies)}`)
+        this.movies.emit(movies);
+      });
+    }
+  }
+
+  public reload(): void {
+    if (this.search.value.trim().length == 0) {
+      let movies : Movie[] = [];
+      this.movieService.all()
       .pipe(take(1)) 
       .subscribe((response: Movie[]) => {
         movies = response.map((movie: Movie) => {
