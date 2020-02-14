ed00dbd1b41c67f8a9923bb4f2750a5960c8979d
Belette <marie.maillos@gmail.com>
2020-02-14 11:32:44 +0100

    guard canActivate

---
+++ src/app/core/guards/login.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  public constructor(
+    private router: Router
+  ) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      this.router.navigate(['home']);
+    return false;
+  }
+  
+}
