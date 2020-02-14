1e65dc98a895ac164a594e98ebdd75ca155857f9
Belette <marie.maillos@gmail.com>
2020-02-14 14:47:42 +0100

    footer avec login user

--- src/app/shared/ui/footer/footer.component.ts
+++ src/app/shared/ui/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/services/user.service';
+import { UserInterface } from 'src/app/core/models/user-interface';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public user: UserInterface;
 
-  constructor() { }
+  constructor( public userService: UserService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.userService.userSubject$.subscribe((user: UserInterface) => {
+      this.user = user;
+    })
   }
 
 }
