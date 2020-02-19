f54ee0991ba0d9c7a2ab57ddcf6bbbfeb2e6bb46
Belette <marie.maillos@gmail.com>
2020-02-19 13:56:31 +0100

    websocket init and heart icon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,12 +9,16 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { take } from 'rxjs/operators';
 import { Movie } from './../../core/models/movie'
 import { Observable, Subscription } from 'rxjs';
+import { WebSocketSubject } from 'rxjs/webSocket';
+
+import { environment } from './../../../environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent {
 
   public title: string = 'movies';
@@ -23,38 +27,58 @@ export class HomeComponent {
   public years: number[] = [];
   private yearSubscription: Subscription;
   public user: UserInterface;
+  private socket$: WebSocketSubject<any>;
+  private timesLiked: number = 0;
 
-public constructor(
-  private movieService: MovieService, 
-  public userService: UserService, 
-  public snackBar: MatSnackBar,
-  private router: Router) { }
+  public constructor(
+    private movieService: MovieService, 
+    public userService: UserService, 
+    public snackBar: MatSnackBar,
+    private router: Router) { }
 
-ngOnInit() {
-  this.movies = this.movieService.all();
-
-  this.yearSubscription = this.movieService.years$
-    .subscribe((_years) => {
-      console.log('Years was updated : ' + JSON.stringify(_years));
-      this.years = _years;
-    })
+  ngOnInit() {
+    this.socket$ = new WebSocketSubject<any>(environment.wssAddress);
+    this.socket$.subscribe((socketMessage: any) => {
+      console.log(`Something comes from wsServer : ${JSON.stringify(socketMessage)}`)
+    },
+    (err) => console.error('Erreur levée : ' + JSON.stringify(err)),
+    () => console.warn('Completed!')
+    );
+    this.socket$.next('Ping');
   
-  this.userService.userSubject$.subscribe((user: UserInterface) => {
-    this.user = user;
-  })
+    this.movies = this.movieService.all();
 
-} 
+    this.yearSubscription = this.movieService.years$
+      .subscribe((_years) => {
+        console.log('Years was updated : ' + JSON.stringify(_years));
+        this.years = _years;
+      })
+  
+    this.userService.userSubject$.subscribe((user: UserInterface) => {
+      this.user = user;
+    })
+
+  }; 
 
   public receiveMovies($event): void {
-    this.movies = $event;
-    console.log(`Received ${JSON.stringify(this.movies)}`);
+  this.movies = $event;
+  console.log(`Received ${JSON.stringify(this.movies)}`);
   };
 
   public snackbarToLogin(idMovie: number) {
     this.snackBar.open('You are not yet connected','',{duration: 1500, verticalPosition: 'top'})
     .afterDismissed().subscribe((a) => {
-      this.router.navigate(['login'], {state: {movie: idMovie}})
+    this.router.navigate(['login'], {state: {movie: idMovie}})
     }
-    )}
+  )};
+
+  public addToList() {
+    console.log(`movie added to wishlist`);
+    this.timesLiked++;
+//this.getParams; 
+//this.router.navigate(['../', 'movie', idMovie])
+  }
+
+  
 
 }
