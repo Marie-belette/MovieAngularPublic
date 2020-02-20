81f557e898dcc9615f1a366dcf06d97caf2c55a3
Belette <marie.maillos@gmail.com>
2020-02-20 13:50:55 +0100

    animation coeur

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,11 +12,38 @@ import { Observable, Subscription } from 'rxjs';
 import { WebSocketSubject } from 'rxjs/webSocket';
 
 import { environment } from './../../../environments/environment';
+import { transition, animate, trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('heartGrowing', [
+      state('initial', style({
+        transform: 'scale(1)',
+        color: 'midnightblue'
+      })),
+      state('final', style({
+        transform: 'scale(2.0)',
+        color: 'pink'
+      })),
+      transition('initial=>final', animate('900ms')),
+      transition('final=>initial', animate('900ms'))
+    ]),
+    trigger('heartSmalling', [
+      state('initial', style({
+        transform: 'scale(2.0)',
+        color: 'pink'
+      })),
+      state('final', style({
+        transform: 'scale(1.0)',
+        color: 'midnightblue'
+      })),
+      transition('initial=>final', animate('900ms')),
+      transition('final=>initial', animate('900ms'))
+    ]),
+  ]
 })
 
 export class HomeComponent {
@@ -93,26 +120,34 @@ export class HomeComponent {
   )};
 
   public likeIt(movie: Movie): void {
-    movie.timesLiked += 1;
-
-    // Emit a new update to ws...
-    const message: any = {
-      message: 'timesLiked',
-      data: movie,
-    };
-    this.socket$.next(message);
-
-    // Update the observable (retains values)
-    this.movies = this.movies.pipe(
-      map((movies: Movie[]): Movie[] => {
-        let movieIndex: number = movies.findIndex(
-          (obj: Movie, index: number) => obj.idMovie == movie.idMovie);
-        movies[movieIndex] = movie;
-        return movies;
-      })
-    );
-  }
-
-  
-
+    movie.animationState = 'final';
+    //setTimeOut(() => {}, delay), donc on indique le délai après les accolades, et dans les accolades tout ce qui doit se passer après ce délai
+    setTimeout(() => {
+      // Emit a new update to ws
+      if (this.user) {
+        movie.timesLiked += 1;
+    
+        // Emit a new update to ws...
+        const message: any = {
+          message: 'timesLiked',
+          data: movie,
+        };
+        this.socket$.next(message);
+    
+        // Update the observable (retains values)
+        this.movies = this.movies.pipe(
+          map((movies: Movie[]): Movie[] => {
+            let movieIndex: number = movies.findIndex(
+              (obj: Movie, index: number) => obj.idMovie == movie.idMovie);
+            movies[movieIndex] = movie;
+            return movies;
+          })
+        );
+      } else {
+        this.snackBar.open('You are not yet connected','',{duration: 1500, verticalPosition: 'top'});
+      };
+      movie.animationState = 'initial';
+      setTimeout(() => movie.animationState = 'final', 900);
+     }, 1000);
+}
 }
